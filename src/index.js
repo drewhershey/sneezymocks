f4f488a372f09d08a6dbcbe4f1b2af86b1a06546
Drew Hershey <drew.i.hershey@gmail.com>
2021-08-06 09:28:44 -0600

    Add comments

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ const { getSkillDam } = require('./getSkillDam');
 
 // Caster/Attacker Config
 const attackerConfig = {
-  casterOrAttackerLevel: 50,
-  skillAdvDiscLearnedness: 100,
-  maxSkillOrSpellLevel: 100, // Sometimes found in code for spell
-  modifierStatValue: 205,
   skillOrSpellName: 'SKILL_BACKSTAB',
-  hasTarget: true,
+  casterOrAttackerLevel: 50,
   targetLevel: 60,
+  // % learnedness of advanced disc for chosen skill
+  skillAdvDiscLearnedness: 100,
+  // Sometimes found in code for individual spells
+  maxSkillOrSpellLevel: 100, 
+  // Attacker's stat value for whichever stat modifies
+  // chosen skill
+  modifierStatValue: 205,
+  // Damage gets adjusted for targetted spells
+  hasTarget: true,
+  // Damage gets adjusted depending on whether
+  // target is PC or mob  
   targetIsPc: false,
 };
 
