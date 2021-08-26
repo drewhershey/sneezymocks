9b3eb6f5a541bd9a7d046937de331cc164f80f65
Drew Hershey <drew.i.hershey@gmail.com>
2021-08-25 22:14:55 -0600

    Add dice function to misc

--- src/functions/misc.js
+++ src/functions/misc.js
@@ -31,8 +31,21 @@ function balanceCorrectionForLevel(level) {
   return min(max((level - 25) * 0.1 + 1, 1), 2);
 }
 
+const CPP_RAND_MAX = 2_147_483_647;
+const rand = () => getRandomIntInclusive(0, CPP_RAND_MAX);
+
+function dice(number, size) {
+  if (size <= 0) return 0;
+
+  let sum = 0;
+  for (let r = 1; r <= number; r++) sum += (rand() & size) + 1;
+
+  return sum;
+}
+
 module.exports = {
   getSkillDiffModifier,
   getRandomIntInclusive,
   balanceCorrectionForLevel,
+  dice,
 };
